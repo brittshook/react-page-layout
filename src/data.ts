26169cf66246d296ad100991ab2f0874a83d44fb
Britt Shook <brittanysshook@gmail.com>
2024-02-09 12:46:35 -0500

    add contact info to data objs

--- src/data.ts
+++ src/data.ts
@@ -2,50 +2,81 @@ type Props = {
   name: string;
   title: string;
   img: string;
+  officePhone: string;
+  mobilePhone: string;
+  email: string;
 };
 
-class Employee {
+export class Employee {
   name: string;
   title: string;
   img: string;
+  officePhone: string;
+  mobilePhone: string;
+  email: string;
 
-  constructor({ name, title, img }: Props) {
+  constructor({ name, title, img, officePhone, mobilePhone, email }: Props) {
     this.name = name;
     this.title = title;
     this.img = img;
+    this.officePhone = officePhone;
+    this.mobilePhone = mobilePhone;
+    this.email = email;
   }
 }
 
-export const james = new Employee({
-  name: "James King",
+const kim = new Employee({
+  name: "Kim Possible",
   title: "President and CEO",
-  img: "",
+  img: "imgs/kim.png",
+  officePhone: "781-000-0001",
+  mobilePhone: "617-000-0001",
+  email: "kim@company.com",
 });
 
-export const julie = new Employee({
-  name: "Julie Taylor",
+const ron = new Employee({
+  name: "Ron Stoppable",
   title: "VP of Marketing",
-  img: "",
+  img: "imgs/ron.png",
+  officePhone: "781-000-0002",
+  mobilePhone: "617-000-0002",
+  email: "ron@company.com",
 });
 
-export const eugene = new Employee({
-  name: "Eugene Lee",
+const monique = new Employee({
+  name: "Monique Lee",
   title: "CFO",
-  img: "",
+  img: "imgs/monique.png",
+  officePhone: "781-000-1233",
+  mobilePhone: "617-000-1233",
+  email: "monique@company.com",
 });
 
-export const john = new Employee({
-  name: "John Williams",
+const shego = new Employee({
+  name: "Shego Williams",
   title: "VP of Engineering",
-  img: "",
+  img: "imgs/shego.png",
+  officePhone: "781-000-2020",
+  mobilePhone: "617-000-2020",
+  email: "john@company.com",
 });
-export const ray = new Employee({
-  name: "Ray Moore",
+
+const rufus = new Employee({
+  name: "Rufus Moore",
   title: "VP of Sales",
-  img: "",
+  img: "imgs/rufus.png",
+  officePhone: "781-000-2999",
+  mobilePhone: "617-000-2999",
+  email: "ray@company.com",
 });
-export const paul = new Employee({
-  name: "Paul Jones",
+
+const wade = new Employee({
+  name: "Wade Jones",
   title: "QA Manager",
-  img: "",
+  img: "imgs/wade.png",
+  officePhone: "781-000-9347",
+  mobilePhone: "617-000-9347",
+  email: "paul@company.com",
 });
+
+export const allEmployees = [kim, ron, monique, shego, rufus, wade];
