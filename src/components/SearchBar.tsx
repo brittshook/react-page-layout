53ce9f90991759ca610144471ecc91a536e6d393
Britt Shook <brittanysshook@gmail.com>
2024-02-09 12:45:25 -0500

    add search bar component and styling

---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,9 @@
+import styles from "./SearchBar.module.css";
+
+export const SearchBar = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.search}></div>
+    </div>
+  );
+};
