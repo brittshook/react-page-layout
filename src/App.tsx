8538e01e58ff00b2a8ac07f6fa44ed495f50fd66
Britt Shook <brittanysshook@gmail.com>
2024-02-09 12:47:03 -0500

    add pages to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import { EmployeePage } from "./pages/EmployeePage";
+import { HomePage } from "./pages/HomePage";
+import { Employee, allEmployees } from "./data";
 
 function App() {
+  const [selectedEmployee, setSelectedEmployee] = useState<
+    Employee | undefined
+  >();
+
+  const handleEmployeeClick = (
+    event: React.MouseEvent<HTMLDivElement, MouseEvent>
+  ) => {
+    const employeeName = event.currentTarget.id;
+    let selectedEmployee;
+    for (const employee of allEmployees) {
+      if (employee.name === employeeName) {
+        selectedEmployee = employee;
+      }
+    }
+    setSelectedEmployee(selectedEmployee);
+  };
+
   return (
-    <>
-    </>
-  )
+    <div id="app">
+      <HomePage onClick={handleEmployeeClick} />
+      <EmployeePage employee={selectedEmployee} />
+    </div>
+  );
 }
 
-export default App
+export default App;
